91dff4680a8f3038bf149115a4ca45160fc690c2
Ting <chienweiting3839@gmail.com>
2023-07-02 20:33:57 +0800

    ADD article(2)

--- main.py
+++ main.py
@@ -75,6 +75,7 @@ def sentence_to_vector(sentence, word_vectors):
 
 
 def compute_similarity_matrix(sentence_vectors):
+    
     num_sentences = len(sentence_vectors)
     similarity_matrix = np.zeros((num_sentences, num_sentences))
 
@@ -92,6 +93,7 @@ def compute_similarity_matrix(sentence_vectors):
 
 
 def textrank(similarity_matrix, d=0.85, max_iter=100, tol=1e-4):
+
     num_sentences = similarity_matrix.shape[0]
     scores = np.ones(num_sentences)  # 初始化權重為1
     old_scores = np.zeros(num_sentences)
@@ -144,7 +146,7 @@ def bart_summary(text):
     # beam search: 生成文本的一種算法，較大的值增加生成文本的多樣性，但會降低生成文本的品質(範圍通常是1~10)
     # max_length=500: 生成的摘要最大長度為500
     # early_stopping=True: 當達到最大長度限制時提前停止生成摘要
-    summary_ids = model.generate(encoded_input['input_ids'], num_beams=4, max_length=500, early_stopping=True)
+    summary_ids = model.generate(encoded_input['input_ids'], num_beams=3, max_length=140, early_stopping=True)
     # print(f'summary_ids: {summary_ids}')
     # summary_ids.squeeze(): 用於刪除在張量中任何多餘的維度
     # skip_special_tokens=True: 讓Decoder忽略特殊token，只返回具有意義的token，來生成最終的摘要
@@ -152,7 +154,8 @@ def bart_summary(text):
     return summary
 
 
-def graph(sentences, similarity_matrix):
+def graph(similarity_matrix):
+
     # 設置字型
     font = {'family': 'SimSun'}
     plt.rc('font', **font)
@@ -172,10 +175,10 @@ def graph(sentences, similarity_matrix):
 
 
 def main():
-    
+
+    # article = '''(CNN)Anthony Ray Hinton is thankful to be free after nearly 30 years on Alabama's death row for murders he says he didn't commit. And incredulous that it took so long. Hinton, 58, looked up, took in the sunshine and thanked God and his lawyers Friday morning outside the county jail in Birmingham, minutes after taking his first steps as a free man since 1985. He spoke of unjustly losing three decades of his life, under fear of execution, for something he didn't do. "All they had to do was to test the gun, but when you think you're high and mighty and you're above the law, you don't have to answer to nobody," Hinton told reporters. "But I've got news for you -- everybody that played a part in sending me to death row, you will answer to God." Jefferson County Circuit Court Judge Laura Petro had ordered Hinton released after granting the state's motion to dismiss charges against him. Hinton was convicted of murder in the 1985 deaths of two Birmingham-area, fast-food restaurant managers, John Davidson and Thomas Wayne Vason. But a new trial was ordered in 2014 after firearms experts testified 12 years earlier that the revolver Hinton was said to have used in the crimes could not be matched to evidence in either case, and the two killings couldn't be linked to each other. "Death Row Stories": Hard questions about the U.S. capital punishment system . The state then declined to re-prosecute the case. Hinton was 29 at the time of the killings and had always maintained his innocence, said the Equal Justice Initiative, a group that helped win his release. "Race, poverty, inadequate legal assistance, and prosecutorial indifference to innocence conspired to create a textbook example of injustice," Bryan Stevenson, the group's executive director and Hinton's lead attorney, said of his African-American client. "I can't think of a case that more urgently dramatizes the need for reform than what has happened to Anthony Ray Hinton." Stevenson said the "refusal of state prosecutors to re-examine this case despite persuasive and reliable evidence of innocence is disappointing and troubling." Amnesty report: Executions down but death sentences on the rise . Dressed in a dark suit and blue shirt, Hinton praised God for his release, saying he was sent "not just a lawyer, but the best lawyers." He said he will continue to pray for the families of the murder victims. Both he and those families have suffered a miscarriage of justice, he said. "For all of us that say that we believe in justice, this is the case to start showing, because I shouldn't have (sat) on death row for 30 years," he said. Woman who spent 22 years on death row has case tossed . Hinton was accompanied Friday by two of his sisters, one of whom still lives in the Birmingham area. Other siblings will fly to the area to see him soon, Stevenson said. His mother, with whom he lived at the time of his arrest, is no longer living, according to the lawyer. Hinton planned to spend at least this weekend at the home of a close friend. He will meet with his attorneys Monday to start planning for his immediate needs, such as obtaining identification and getting a health checkup, Stevenson said. The plan now is to spend a few weeks to get oriented with freedom and "sort out what he wants to do," Stevenson said.'''
+    article = '''(CNN)Five Americans who were monitored for three weeks at an Omaha, Nebraska, hospital after being exposed to Ebola in West Africa have been released, a Nebraska Medicine spokesman said in an email Wednesday. One of the five had a heart-related issue on Saturday and has been discharged but hasn't left the area, Taylor Wilson wrote. The others have already gone home. They were exposed to Ebola in Sierra Leone in March, but none developed the deadly virus. They are clinicians for Partners in Health, a Boston-based aid group. They all had contact with a colleague who was diagnosed with the disease and is being treated at the National Institutes of Health in Bethesda, Maryland. As of Monday, that health care worker is in fair condition. The Centers for Disease Control and Prevention in Atlanta has said the last of 17 patients who were being monitored are expected to be released by Thursday. More than 10,000 people have died in a West African epidemic of Ebola that dates to December 2013, according to the World Health Organization. Almost all the deaths have been in Guinea, Liberia and Sierra Leone. Ebola is spread by direct contact with the bodily fluids of an infected person.'''
     # sentence tokenization
-    article = '''(CNN)Anthony Ray Hinton is thankful to be free after nearly 30 years on Alabama's death row for murders he says he didn't commit. And incredulous that it took so long. Hinton, 58, looked up, took in the sunshine and thanked God and his lawyers Friday morning outside the county jail in Birmingham, minutes after taking his first steps as a free man since 1985. He spoke of unjustly losing three decades of his life, under fear of execution, for something he didn't do. "All they had to do was to test the gun, but when you think you're high and mighty and you're above the law, you don't have to answer to nobody," Hinton told reporters. "But I've got news for you -- everybody that played a part in sending me to death row, you will answer to God." Jefferson County Circuit Court Judge Laura Petro had ordered Hinton released after granting the state's motion to dismiss charges against him. Hinton was convicted of murder in the 1985 deaths of two Birmingham-area, fast-food restaurant managers, John Davidson and Thomas Wayne Vason. But a new trial was ordered in 2014 after firearms experts testified 12 years earlier that the revolver Hinton was said to have used in the crimes could not be matched to evidence in either case, and the two killings couldn't be linked to each other. "Death Row Stories": Hard questions about the U.S. capital punishment system . The state then declined to re-prosecute the case. Hinton was 29 at the time of the killings and had always maintained his innocence, said the Equal Justice Initiative, a group that helped win his release. "Race, poverty, inadequate legal assistance, and prosecutorial indifference to innocence conspired to create a textbook example of injustice," Bryan Stevenson, the group's executive director and Hinton's lead attorney, said of his African-American client. "I can't think of a case that more urgently dramatizes the need for reform than what has happened to Anthony Ray Hinton." Stevenson said the "refusal of state prosecutors to re-examine this case despite persuasive and reliable evidence of innocence is disappointing and troubling." Amnesty report: Executions down but death sentences on the rise . Dressed in a dark suit and blue shirt, Hinton praised God for his release, saying he was sent "not just a lawyer, but the best lawyers." He said he will continue to pray for the families of the murder victims. Both he and those families have suffered a miscarriage of justice, he said. "For all of us that say that we believe in justice, this is the case to start showing, because I shouldn't have (sat) on death row for 30 years," he said. Woman who spent 22 years on death row has case tossed . Hinton was accompanied Friday by two of his sisters, one of whom still lives in the Birmingham area. Other siblings will fly to the area to see him soon, Stevenson said. His mother, with whom he lived at the time of his arrest, is no longer living, according to the lawyer. Hinton planned to spend at least this weekend at the home of a close friend. He will meet with his attorneys Monday to start planning for his immediate needs, such as obtaining identification and getting a health checkup, Stevenson said. The plan now is to spend a few weeks to get oriented with freedom and "sort out what he wants to do," Stevenson said.'''
-    #text = re.sub(f'[{string.punctuation}]', '', article)
     sentences = sent_tokenize(article)
     print(f'總共有: {len(sentences)} 個句子')
     preprocessed_sentences = [preprocess_sentence(sentence) for sentence in sentences]
@@ -204,7 +207,9 @@ def main():
     num_sentences= round(len(sentences) * 0.1)
     #num_sentences = 27
     top_indices = sorted_indices[:num_sentences]
-    textrank_summary = [sentences[i] for i in top_indices]
+    textrank_summary = [str(sentences[i]) for i in top_indices]
+    print(f'textrank_summary: {textrank_summary}')
+    # preprocessed_sentences = [preprocess_sentence(sentence) for sentence in sentences]
 
     for i, sentence in enumerate(textrank_summary):
         score = summary_scores[top_indices[i]]
@@ -220,16 +225,14 @@ def main():
     final_summary = bart_summary(bart)
 
     merged_summary = list(set(textrank_summary + bart))
+    preprocessed_sentences = merged_summary
     print(f'merged_summary: {merged_summary}')
 
     print(f'final_summary: {final_summary}')
 
 
 
-    graph(sentences, similarity_matrix)
-
-
-    # sentence_level_word_embedding(article)
+    graph(similarity_matrix)
 
 
 if __name__ == '__main__':
